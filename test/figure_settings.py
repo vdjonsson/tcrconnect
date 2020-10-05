7709f3112704ac1a41e885e21a45adfa361c4142
racng <rachelng323@gmail.com>
2020-10-05 14:57:34 -0700

    clean up files

--- test/figure_settings.py
+++
@@ -1,7 +0,0 @@
-import seaborn as sns
-from matplotlib import rcParams
-
-sns.set_context("paper")
-rcParams['figure.dpi'] = 300
-rcParams['savefig.transparent'] = True
-rcParams['savefig.dpi'] = 300
